40c0f8d07ff581fc6d7571fec5abf6609cba2888
gabrieledigiulio <g.digiulio3@studenti.unipi.it>
2025-08-21 11:43:11 +0200

    Update lesson15: nuovi file e modifiche

--- lesson15/lezione15.c
+++ lesson15/lezione15.c
@@ -10,7 +10,7 @@ every node register his value and in the left we got value with a minor value an
 struct node {
     int val;
     struct node *left, *right;
-}
+};
 
 /*
 * Add a new node if root is NULL returns the new root 
@@ -19,12 +19,46 @@ struct node {
 * 
 */
 struct node *add(struct node *root, int val) {
-    struct node *new = malloc(sizeof(*new)); //
+    struct node *saved_root = root;
+    struct node *new = malloc(sizeof(*new)); //We dont check for malloc() failures this is a trivial exemple
+    new->left = new->right= NULL;
+    new->val = val;
 
+    if(root == NULL) return new;
+
+    while(1) {
+        if (val > root->val) {
+            if(root->right == NULL) {
+                root->right = new;
+                return saved_root;
+            }
+            root = root->right;
+        }else{
+            if (root->left == NULL) {
+                root->left = new;
+                return saved_root;
+            }
+            root = root->left;
+        }
+    }
+
+};
+
+void print_sorted(struct node *root) {
+    if (root == NULL) return;
+    print_sorted(root->left);
+    printf("%d\n", root->val);
+    print_sorted(root->right);
 }
 
 int main(void) {
     struct node *root = NULL;
     root = add(root, 10);
+    root = add(root, 5);
+    root = add(root, 20);
+    root = add(root, 100);
+    root = add(root, 40);
+    root = add(root, 33);
+    print_sorted(root);
     return 0;
 }
